f3416016cd839adaefa4653c625b7a4b67d34b57
alexmon1989 <alex.mon1989@gmail.com>
2017-10-03 17:12:05 +0300

    feature: url для home-страницы.

--- afliga/urls.py
+++ afliga/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.conf import settings
 from django.contrib import admin
+from home import views as home_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_views.home_page, name='home'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
