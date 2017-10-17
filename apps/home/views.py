88f28fb08becd78cdd3931ca8a627af887d264f0
alexmon1989 <alex.mon1989@gmail.com>
2017-10-17 16:06:11 +0300

    feature: список последних новостей на главной странице.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from apps.news.models import News
 
 
 def home_page(request):
     """Отображает главную страницу."""
-    return render(request, 'home/home.html')
+    return render(request, 'home/home.html', {
+        'last_news': News.objects.filter(is_visible=True).order_by('-created_at')[:4]
+    })
