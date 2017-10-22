01dfd7ed853969f5b31fa0cf112d6575a5f81a8a
Alex Monastyretsky <alex.mon1989@gmail.com>
2017-10-22 13:03:53 +0300

    refactoring: уменьшение количества запросов к БД.

--- apps/home/views.py
+++ apps/home/views.py
@@ -4,6 +4,7 @@ from apps.news.models import News
 
 def home_page(request):
     """Отображает главную страницу."""
+    last_news = News.objects.filter(is_visible=True).order_by('-created_at').all()[:4]
     return render(request, 'home/home.html', {
-        'last_news': News.objects.filter(is_visible=True).order_by('-created_at')[:4]
+        'last_news': last_news
     })
