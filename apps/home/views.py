fddb4a52bce465b050e44d6ec47443b2123cdff8
Alex Monastyretsky <alex.mon1989@gmail.com>
2017-10-24 20:51:21 +0300

    feature: последние видео на главной странице.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from apps.news.models import News
+from apps.videogallery.models import Video
 
 
 def home_page(request):
     """Отображает главную страницу."""
     last_news = News.objects.filter(is_visible=True).order_by('-created_at').all()[:4]
+    last_videos = Video.objects.filter(is_visible=True).order_by('-created_at').all()[:3]
+
     return render(request, 'home/home.html', {
-        'last_news': last_news
+        'last_news': last_news,
+        'last_videos': last_videos,
     })
