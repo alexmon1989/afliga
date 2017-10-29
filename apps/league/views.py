5bf592d5b79951de61b69865c1caddc05863f8dc
Alex Monastyretsky <alex.mon1989@gmail.com>
2017-10-29 13:01:53 +0200

    refactoring: структура каталогов.

--- apps/league/views.py
+++ apps/league/views.py
@@ -5,14 +5,14 @@ from apps.league.models import Tournament
 class TournamentListView(ListView):
     """Отображает страницу со списком турниров."""
     model = Tournament
-    template_name = 'league/tournaments/list.html'
+    template_name = 'league/tournaments/list/list.html'
     queryset = Tournament.objects.order_by('-created_at')
 
 
 class TournamentDetailView(DetailView):
     """Отображает страницу с турнира."""
     model = Tournament
-    template_name = 'league/tournaments/detail.html'
+    template_name = 'league/tournaments/detail/detail.html'
 
     def get_context_data(self, **kwargs):
         context = super(TournamentDetailView, self).get_context_data(**kwargs)
