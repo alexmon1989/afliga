1a7efd9a287a443f72560afb3b22b7e9b5dcfb54
alexmon1989 <alex.mon1989@gmail.com>
2017-11-01 13:58:55 +0200

    feature: ссылки на команды и игроков.

--- apps/league/urls.py
+++ apps/league/urls.py
@@ -3,7 +3,7 @@ from apps.league.views import TournamentListView, TournamentDetailView, TeamDeta
 
 urlpatterns = [
     url(r'^$', TournamentListView.as_view(), name='tournaments_list'),
-    url(r'^tournament/(?P<pk>\d+)/$', TournamentDetailView.as_view(), name='tournament_detail'),
-    url(r'^team/(?P<pk>\d+)/$', TeamDetailView.as_view(), name='team_detail'),
-    url(r'^player/(?P<pk>\d+)/$', PlayerDetailView.as_view(), name='player_detail'),
+    url(r'^tournament/(?P<pk>\d+)$', TournamentDetailView.as_view(), name='tournament_detail'),
+    url(r'^team/(?P<pk>\d+)$', TeamDetailView.as_view(), name='team_detail'),
+    url(r'^player/(?P<pk>\d+)$', PlayerDetailView.as_view(), name='player_detail'),
 ]
