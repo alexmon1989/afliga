18bbf627af57b56b190e3b77d0af1028205dae60
alexmon1989 <alex.mon1989@gmail.com>
2017-11-02 17:08:45 +0200

    feature: заготовка страницы матча.

--- apps/league/urls.py
+++ apps/league/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from apps.league.views import TournamentListView, TournamentDetailView, TeamDetailView, PlayerDetailView
+from apps.league.views import (TournamentListView, TournamentDetailView, TeamDetailView, PlayerDetailView,
+                               MatchDetailView)
 
 urlpatterns = [
     url(r'^$', TournamentListView.as_view(), name='tournaments_list'),
     url(r'^tournament/(?P<pk>\d+)$', TournamentDetailView.as_view(), name='tournament_detail'),
     url(r'^team/(?P<pk>\d+)$', TeamDetailView.as_view(), name='team_detail'),
     url(r'^player/(?P<pk>\d+)$', PlayerDetailView.as_view(), name='player_detail'),
+    url(r'^match/(?P<pk>\d+)$', MatchDetailView.as_view(), name='match_detail'),
 ]
