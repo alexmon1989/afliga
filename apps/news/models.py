e3ebc648d5a772ec12ca744d9060eddc1bbeaeae
alexmon1989 <alex.mon1989@gmail.com>
2017-11-06 15:56:50 +0200

    feature: ограничение размера картинок новостей при выводе страниц.

--- apps/news/models.py
+++ apps/news/models.py
@@ -7,7 +7,13 @@ class News(models.Model):
     title = models.CharField('Заголовок', max_length=255, blank=False)
     text = RichTextUploadingField('Текст', blank=False)
     is_visible = models.BooleanField('Включено', default=True)
-    image = models.ImageField('Изображение', upload_to='news', null=True, blank=True)
+    image = models.ImageField(
+        'Изображение',
+        upload_to='news',
+        null=True,
+        blank=True,
+        help_text='Оптимальный размер: 740px*380px.'
+    )
     created_at = models.DateTimeField('Создано', auto_now_add=True)
     updated_at = models.DateTimeField('Обновлено', auto_now=True)
 
