b0f1fce9208a6f5a0c9e39b243a3dd3278da8e00
alexmon1989 <alex.mon1989@gmail.com>
2017-11-06 11:01:54 +0200

    feature: пересчёт результатов турнирной таблицы.

--- apps/league/admin.py
+++ apps/league/admin.py
@@ -77,9 +77,69 @@ class EventInline(admin.StackedInline):
     exclude = ('table',)
 
 
+class MatchForm(forms.ModelForm):
+    """Класс формы для редактирования матча."""
+    recount_results = forms.BooleanField(
+        label='Пересчитать результаты в таблице группы',
+        required=False,
+        help_text='Если это групповой матч, то произведётся пересчёт очков у команд. '
+                  'Также можно отредактировать вручную, '
+                  'для этого необходимо перейти в интерфейс администрирования группы.'
+    )
+
+    def save(self, commit=True):
+        """Сохранение данных формы."""
+        recount_results = self.cleaned_data.get('recount_results', False)
+        # Если необходимо пересчитать результаты в таблице группы
+        if recount_results:
+            if self.instance.group:
+                group_teams = self.instance.group.teams.all()
+                team_1 = self.instance.team_1
+                team_2 = self.instance.team_2
+                if team_1 in group_teams and team_2 in group_teams:
+                    group_data = json.loads(self.instance.group.table)
+
+                    goals_team_1 = self.instance.get_goals_team_1()
+                    goals_team_2 = self.instance.get_goals_team_2()
+
+                    for item in group_data:
+                        # Пересчёт результатов команды хозяев
+                        if item['id'] == team_1.pk:
+                            item['goals_scored'] += goals_team_1
+                            item['goals_missed'] += goals_team_2
+                            if goals_team_1 > goals_team_2:
+                                item['wins'] += 1
+                                item['score'] += 3
+                            elif goals_team_1 == goals_team_2:
+                                item['draws'] += 1
+                                item['score'] += 1
+                            else:
+                                item['defeats'] += 1
+
+                        # Пересчёт результатов команды гостей
+                        if item['id'] == team_2.pk:
+                            item['goals_scored'] += goals_team_2
+                            item['goals_missed'] += goals_team_1
+                            if goals_team_2 > goals_team_1:
+                                item['wins'] += 1
+                                item['score'] += 3
+                            elif goals_team_1 == goals_team_2:
+                                item['draws'] += 1
+                                item['score'] += 1
+                            else:
+                                item['defeats'] += 1
+
+                    # Сохранение обновлённой таблицы
+                    self.instance.group.table = json.dumps(group_data)
+                    self.instance.group.save()
+
+        return super(MatchForm, self).save(commit=commit)
+
+
 @admin.register(Match)
 class MatchesAdmin(admin.ModelAdmin):
     """Класс для описания интерфейса администрирования матчей."""
+    form = MatchForm
     list_display = ('teams', 'score', 'match_round', 'group', 'tournament', 'match_date', 'created_at', 'updated_at')
     ordering = ('-created_at',)
     inlines = (EventInline,)
